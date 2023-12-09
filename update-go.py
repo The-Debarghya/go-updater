4e939f4c61a57e8672a6b0435e6ee2dd13e18fac
The-Debarghya <debarghya4444@gmail.com>
2023-12-09 11:44:09 +0530

    add update logic

--- update-go.py
+++ update-go.py
@@ -14,12 +14,24 @@ def verify_version(version: str) -> bool:
         return version != get_current_version()
     return True
 
+def download_new_archive(version: str, arch: str, os: str):
+    pass
+    
+def remove_old_version():
+    pass
+
+def unpack_archive(version: str, arch: str, os: str):
+    pass
+
+def update_symlinks(version: str):
+    pass
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Update go version')
     parser.add_argument('-v', '--version', help='Update to version', required=True)
-    parser.add_argument('-a', '--arch', help='Update for arch [Default AMD64]', default='x86_64')
-    parser.add_argument('-o', '--os', help='Update for os [Default Linux]', default='linux')
+    parser.add_argument('-a', '--arch', help='Update for arch [Default AMD64]', default='amd64', choices=['amd64', 'arm64', '386', 'armv6l', 'ppc64le', 's390x'])
+    parser.add_argument('-o', '--os', help='Update for os [Default Linux]', default='linux', choices=['linux', 'darwin', 'windows', 'freebsd'])
     version = get_current_version()
     parser.add_argument('-c', '--current', help='Get current version status', action='version', version=version)
 
@@ -27,4 +39,9 @@ if __name__ == '__main__':
     if not verify_version(args.version):
         print(f'Already on version {args.version}')
         exit(0)
+
+    download_new_archive(args.version, args.arch, args.os)
+    remove_old_version()
+    unpack_archive(args.version, args.arch, args.os)
+    update_symlinks(args.version)
     
